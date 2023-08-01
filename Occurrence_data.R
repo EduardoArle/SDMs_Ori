7f64730e058d093d7f831a860f8ebd351576a04d
EduardoArle <eduardo.arle@idiv.de>
2023-08-01 13:09:48 +0300

    20230801

---
+++ Occurrence_data.R
@@ -0,0 +1,21 @@
+# load packages
+library(sf)
+
+# list relevant WDs
+wd_occ <- "/Users/carloseduardoaribeiro/Documents/Collaborations/Ori/Occurrence"
+wd_shp <-  "/Users/carloseduardoaribeiro/Documents/Collaborations/Ori/Shapefile"
+
+# load shapefiles
+ecoregions <- st_read(dsn = wd_shp, layer = "ecoregions")
+
+# load occurrence data
+setwd(wd_occ) 
+occ <- read.csv("habitat data isr.csv")
+
+# make spatial object from occurrence data
+occ_sp <- st_as_sf(occ, coords = c('cordinate.lon', 'cordinate.lat'),
+                      crs = st_crs(ecoregions))
+
+# visualise the data
+plot(st_geometry(ecoregions), col = 'azure') #plot region
+plot(occ_sp, add =T, pch = 19, col = 'red') #plot points
